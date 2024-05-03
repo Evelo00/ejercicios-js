1df054cda42619c83747d29f10d879ebc4157d0b
Eyver Vergara <eveloxD@gmail.com>
2024-05-03 01:42:09 -0500

    Commit final

--- Ejercicios bucles/app.js
+++ Ejercicios bucles/app.js
@@ -1,4 +1,3 @@
-/*
 
 // Realizar un programa que permita el ingreso de un numero y muestre su tabla de
 // multiplicar (Los primeros 10 multiplos).
@@ -227,7 +226,6 @@ do {
     }
 } while (ganador === "Empate");
 
-*/
 
 
 
