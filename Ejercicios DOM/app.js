f18ed3550740e0e73af2b65e590d26bf123e663c
Eyver Vergara <eveloxD@gmail.com>
2024-05-07 01:57:05 -0500

    solucion del conversor, flata 3

--- Ejercicios DOM/app.js
+++ Ejercicios DOM/app.js
@@ -5,9 +5,10 @@ let resultadoDiv = document.getElementById('resultado');
 
 //conversor de monedas
 let dolaresInput = document.getElementById("dolares");
-let pesosInput = document.getElementById("pesos");
+let pesosColInput = document.getElementById("pesos");
 let convertirDolaresButton = document.getElementById("convertirDolares");
-let conversionDolaresDiv = document.getElementById("conversionDolares");
+let convertirPesosButton = document.getElementById("convertirPesos");
+let conversionDiv = document.getElementById("conversion");
 let tasaDeCambio = 0.00027;
 
 calcularButton.disabled = true;
@@ -27,27 +28,28 @@ calcularButton.addEventListener('click', function() {
     resultadoDiv.textContent = 'Tu IMC es ' + imc.toFixed(2);
 });
 
-
 // conversor
-
 
 convertirDolaresButton.addEventListener("click", function () {
     let dolares = dolaresInput.value;
-    let pesos = pesosInput.value;
 
-    if (!dolares && !pesos) {
-        conversionDolaresDiv.textContent =
-            "Por favor, introduce una cantidad en dólares o en pesos.";
+    if (!dolares) {
+        conversionDiv.textContent = "Por favor, introduce una cantidad en dólares.";
         return;
     }
 
-  if (!dolares) {
-    conversionDolaresDiv.textContent =
-      "Por favor, introduce una cantidad en dólares.";
-    return;
-  }
+    let pesos = dolares / tasaDeCambio;
+    conversionDiv.textContent = "La cantidad en pesos colombianos es " + pesos.toFixed(2);
+});
 
-  let pesos = dolares / tasaDeCambio;
-  conversionDolaresDiv.textContent =
-    "La cantidad en pesos colombianos es " + pesos.toFixed(2);
+convertirPesosButton.addEventListener("click", function () {
+    let pesos = pesosColInput.value;
+
+    if (!pesos) {
+        conversionDiv.textContent = "Por favor, introduce una cantidad en pesos colombianos.";
+        return;
+    }
+
+    let dolares = pesos * tasaDeCambio;
+    conversionDiv.textContent = "La cantidad en dólares es " + dolares.toFixed(2);
 });
